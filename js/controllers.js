9f5c55fa6dcc5467aa2412bc27c262d4c9a9ab56
Miles Oberstadt <milesof2007@gmail.com>
2015-05-03 12:05:42 -0500

    Move box creation to a directive

--- js/controllers.js
+++ js/controllers.js
@@ -15,3 +15,23 @@ tweetboardApp.controller('TweetBoardCtrl', function ($scope, $http) {
 
 	$scope.loadBoxes();
 });
+
+tweetboardApp.directive('boxHolder', function($compile){
+	return {
+		restrict: 'AE',
+		template:
+			  '<div class="box-small" ng-repeat="box in boxes">'
+			+ '	<img style="width=50px" ng-src="{{box.avatar}}">'
+			+ '	<div class="accountInfo">'
+			+ '		<strong class="fullname">{{box.full_name}}</strong>'
+			+ '		<br><span class="username">@<b>{{box.username}}</b></span>'
+			+ '	</div>'
+			+ '	<div class="smallTextHolder">'
+			+ '		<p class="tweet-text">{{box.tweet.text}}</p>'
+			+ '	</div>'
+			+ '</div>',
+		link: function($scope, $element){
+			//Put Javascript here that you want to interact with the above generated directive
+		}
+	};
+});
